474caa6125eec3d668195ae6079433aa69c972c8
ml <30815731+slimlime@users.noreply.github.com>
2021-02-14 16:49:29 +1000

    build: merge plugin (package) template over package
    Web platform, plugin template; through `flutter create --template plugin --platforms web .`
    
    BREAKING CHANGE: Would potentially break people if anyone was using this blank repo

--- test/flutter_qr_scanner_test.dart
+++ test/flutter_qr_scanner_test.dart
@@ -1,12 +1,23 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_qr_scanner/flutter_qr_scanner.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  const MethodChannel channel = MethodChannel('flutter_qr_scanner');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await FlutterQrScanner.platformVersion, '42');
   });
 }
